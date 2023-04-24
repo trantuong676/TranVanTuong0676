e464a4016d9ed10885cc84caea2ecfbd7786c4cf
trantuong21 <trantuong21022001@gmail.com>
2023-04-24 15:50:08 +0700

    first commit

---
+++ tranvantuong0676/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tranvantuong0676.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
